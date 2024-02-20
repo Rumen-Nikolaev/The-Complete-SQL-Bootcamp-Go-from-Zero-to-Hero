8af22e06ab1582beb667988a41fa7e62fd90def8
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-02-20 11:53:03 +0200

    Create 01:AggregationFunctions.sql

---
+++ Section 3: GROUP BY Statements/01:AggregationFunctions.sql
@@ -0,0 +1,13 @@
+SELECT MAX(replacement_cost) FROM film;
+
+SELECT MAX(replacement_cost), MIN(replacement_cost)
+FROM film;
+
+SELECT AVG(replacement_cost)
+FROM film;
+
+SELECT ROUND(AVG(replacement_cost), 4)
+FROM film;
+
+SELECT SUM(replacement_cost)
+FROM film;
